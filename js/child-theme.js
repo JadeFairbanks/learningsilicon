53aa8b60357e0454dfe2c49368ae388fae88a8a1
JadeFairbanks <jadefairbanks98@gmail.com>
2023-06-14 16:08:52 -0700

    updated the binary to no longer moving in preparation for next steps

--- js/child-theme.js
+++ js/child-theme.js
@@ -6743,114 +6743,62 @@
 	  }
 	})();
 
-	// Get the canvas element by its ID
 	const canvas = document.getElementById('binaryCanvas');
-
-	// Get the 2D rendering context for the drawing surface of the canvas
 	const ctx = canvas.getContext('2d');
-
-	// This is the text to render - a string containing '10'
 	const binary = '10';
+	let binaryRows = [];
 	function getBaseFontSize() {
-	  // Create a temporary element with a size of 1em
 	  let temp = document.createElement('div');
 	  temp.style.width = "1em";
 	  temp.style.height = "1em";
 	  document.body.appendChild(temp);
-
-	  // Get the size of the element in pixels
 	  const size = temp.offsetWidth;
-
-	  // Remove the temporary element
 	  document.body.removeChild(temp);
 	  return size;
 	}
 	const baseFontSize = getBaseFontSize();
+	const fontSize = 1.5 * baseFontSize;
+	const color = "#19F059";
 
-	// Define your new font size in ems, converted to pixels
-	const fontSize = 1.5 * baseFontSize; // 1.5em
-
-	// Array to keep track of drop positions for each column
-	let drops = [];
-
-	// Array of green shades for color cycling
-	const colors = ["#19F059", "#1BC75B", "#1DEF5D", "#20FF60"]; // Add more shades if needed
-
-	// Index to keep track of which color to use
-	let colorIndex = 0;
-	let intervalId;
 	function init() {
-	  // Clear any running interval
-	  if (intervalId) {
-	    clearInterval(intervalId);
-	  }
-
-	  // Set the width and height of the canvas
 	  canvas.width = window.innerWidth;
-	  canvas.height = 30 * 16; // to match your CSS height 30rem
-
-	  // Calculate the number of columns in the canvas by dividing its width by the font size
-	  const columns = Math.floor(canvas.width / fontSize);
-
-	  // Debug: log the calculated columns
-	  console.log(`Columns calculated: ${columns}`);
-
-	  // Check if columns are negative or NaN, if so log the values and prevent execution
-	  if (isNaN(columns) || columns < 0) {
-	    console.error('Invalid columns calculation', {
+	  canvas.height = window.innerHeight;
+	  const columns = Math.ceil(canvas.width / fontSize); // We change floor to ceil for columns as well
+	  const rows = Math.ceil(canvas.height / fontSize);
+	  if (isNaN(columns) || isNaN(rows) || columns < 0 || rows < 0) {
+	    console.error('Invalid dimensions calculation', {
 	      columns,
+	      rows,
 	      width: canvas.width,
+	      height: canvas.height,
 	      fontSize
 	    });
 	    return;
 	  }
-
-	  // Initialize or reset all drops positions to 1
-	  drops = Array(columns).fill(1);
-
-	  // Call the draw function every 33 milliseconds to create the animation
-	  intervalId = setInterval(draw, 33);
+	  binaryRows = Array.from({
+	    length: rows
+	  }, () => Array.from({
+	    length: columns
+	  }, () => binary.charAt(Math.floor(Math.random() * binary.length))));
+	  draw();
 	}
-
-	// Function to draw each frame
 	function draw() {
-	  // Set the color to black with a slight transparency
-	  ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
-
-	  // Draw a rectangle over the entire canvas to create the trailing effect
+	  ctx.fillStyle = 'rgba(0, 0, 0, 1)';
 	  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-	  // Set the color for the text
-	  ctx.fillStyle = colors[colorIndex];
-
-	  // Set the font size and family for the text
+	  ctx.fillStyle = color;
 	  ctx.font = fontSize + 'px Georgia';
-
-	  // Loop over each drop
-	  for (let i = 0; i < drops.length; i++) {
-	    // Choose a random character from the binary string
-	    const text = binary.charAt(Math.floor(Math.random() * binary.length));
-
-	    // Draw the text at the specified position
-	    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-
-	    // If the drop has reached the bottom of the canvas, reset it to the top
-	    // We use a random number to create some randomness in the reset timing
-	    if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) drops[i] = 0;
-
-	    // Move the drop down
-	    drops[i]++;
+	  ctx.textBaseline = 'top';
+	  ctx.imageSmoothingEnabled = false;
+	  for (let i = 0; i < binaryRows.length; i++) {
+	    for (let j = 0; j < binaryRows[i].length; j++) {
+	      ctx.fillText(binaryRows[i][j], j * fontSize, i * fontSize);
+	    }
 	  }
-
-	  // Cycle to the next color
-	  colorIndex = (colorIndex + 1) % colors.length;
 	}
-
-	// Listen for the resize event
 	window.addEventListener('resize', init);
-
-	// Initial setup
 	init();
+	console.log("debug: " + binaryRows[1]);
+	console.log("debug: " + binaryRows[2]);
 
 	exports.Alert = alert;
 	exports.Button = button;
--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,111 +1,76 @@
-// Get the canvas element by its ID
 const canvas = document.getElementById('binaryCanvas');
-
-// Get the 2D rendering context for the drawing surface of the canvas
 const ctx = canvas.getContext('2d');
-
-// This is the text to render - a string containing '10'
 const binary = '10';
+let binaryRows = [];
 
 function getBaseFontSize() {
-    // Create a temporary element with a size of 1em
     let temp = document.createElement('div');
     temp.style.width = "1em";
     temp.style.height = "1em";
     document.body.appendChild(temp);
-
-    // Get the size of the element in pixels
     const size = temp.offsetWidth;
-
-    // Remove the temporary element
     document.body.removeChild(temp);
-
     return size;
 }
 
 const baseFontSize = getBaseFontSize();
+const fontSize = 1.5 * baseFontSize;
+const color = "#19F059";
 
-// Define your new font size in ems, converted to pixels
-const fontSize = 1.5 * baseFontSize; // 1.5em
+function interpolateColors(color1, color2) {
+    // Extract RGBA values
+    const extractRGBA = (color) => color.match(/\d+\.?\d*/g).map(Number);
+    const [r1, g1, b1, a1] = extractRGBA(color1);
+    const [r2, g2, b2, a2] = extractRGBA(color2);
 
-// Array to keep track of drop positions for each column
-let drops = [];
+    // Function to generate a random number between two numbers
+    const randomBetween = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-// Array of green shades for color cycling
-const colors = ["#19F059", "#1BC75B", "#1DEF5D", "#20FF60"]; // Add more shades if needed
+    // Generate new RGBA values
+    const r = randomBetween(Math.min(r1, r2), Math.max(r1, r2));
+    const g = randomBetween(Math.min(g1, g2), Math.max(g1, g2));
+    const b = randomBetween(Math.min(b1, b2), Math.max(b1, b2));
+    const a = Math.random() * (Math.max(a1, a2) - Math.min(a1, a2)) + Math.min(a1, a2);
 
-// Index to keep track of which color to use
-let colorIndex = 0;
+    // Return new color
+    return `rgba(${r}, ${g}, ${b}, ${a})`;
+    // ctx.fillStyle = interpolateColors('rgba(33, 255, 144, 1)', 'rgba(102, 255, 0, 1)')
+}
 
-let intervalId;
 
 function init() {
-    // Clear any running interval
-    if(intervalId) {
-        clearInterval(intervalId);
-    }
-
-    // Set the width and height of the canvas
     canvas.width = window.innerWidth;
-    canvas.height = 30 * 16; // to match your CSS height 30rem
+    canvas.height = window.innerHeight;
 
-    // Calculate the number of columns in the canvas by dividing its width by the font size
-    const columns = Math.floor(canvas.width / fontSize);
+    const columns = Math.ceil(canvas.width / fontSize); // We change floor to ceil for columns as well
+    const rows = Math.ceil(canvas.height / fontSize); 
 
-    // Debug: log the calculated columns
-    console.log(`Columns calculated: ${columns}`);
-
-    // Check if columns are negative or NaN, if so log the values and prevent execution
-    if (isNaN(columns) || columns < 0) {
-        console.error('Invalid columns calculation', {columns, width: canvas.width, fontSize});
+    if (isNaN(columns) || isNaN(rows) || columns < 0 || rows < 0) {
+        console.error('Invalid dimensions calculation', { columns, rows, width: canvas.width, height: canvas.height, fontSize });
         return;
     }
 
-    // Initialize or reset all drops positions to 1
-    drops = Array(columns).fill(1);
-
-    // Call the draw function every 33 milliseconds to create the animation
-    intervalId = setInterval(draw, 33);
+    binaryRows = Array.from({ length: rows }, () => Array.from({ length: columns }, () => binary.charAt(Math.floor(Math.random() * binary.length))));
+    draw();
 }
 
-
-// Function to draw each frame
 function draw() {
-    // Set the color to black with a slight transparency
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
-
-    // Draw a rectangle over the entire canvas to create the trailing effect
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    // Set the color for the text
-    ctx.fillStyle = colors[colorIndex];
-
-    // Set the font size and family for the text
+    ctx.fillStyle = color;
     ctx.font = fontSize + 'px Georgia';
+    ctx.textBaseline = 'top'; 
+    ctx.imageSmoothingEnabled = false; 
 
-    // Loop over each drop
-    for(let i = 0; i < drops.length; i++) {
-        // Choose a random character from the binary string
-        const text = binary.charAt(Math.floor(Math.random()*binary.length));
-
-        // Draw the text at the specified position
-        ctx.fillText(text, i*fontSize, drops[i]*fontSize);
-        
-        // If the drop has reached the bottom of the canvas, reset it to the top
-        // We use a random number to create some randomness in the reset timing
-        if(drops[i]*fontSize > canvas.height && Math.random() > 0.975)
-            drops[i] = 0;
-
-        // Move the drop down
-        drops[i]++;
+    for (let i = 0; i < binaryRows.length; i++) {
+        for (let j = 0; j < binaryRows[i].length; j++) {
+            ctx.fillText(binaryRows[i][j], j * fontSize, i * fontSize);
+        }
     }
-
-    // Cycle to the next color
-    colorIndex = (colorIndex + 1) % colors.length;
 }
 
-// Listen for the resize event
 window.addEventListener('resize', init);
-
-// Initial setup
 init();
+
+console.log("debug: " + binaryRows[1]);
+console.log("debug: " + binaryRows[2]);
